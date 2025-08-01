414921877369c6840f009408081ca91995b96a76
Your Name <your-email@example.com>
2025-08-01 23:55:18 +0300

    optimized

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,22 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   resolve: {
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500, // increases limit to 1.5 MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'], // vendor chunk
+
+        }
+      }
     }
   }
 })
